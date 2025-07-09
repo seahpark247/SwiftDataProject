7372e3884a0440edb99a7942f8de3babcd088385
seahpark247 <famousjedo@gmail.com>
2025-07-09 17:18:24 -0500

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Seah Park on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDataProjectTests/SwiftDataProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectTests.swift
+//  SwiftDataProjectTests
+//
+//  Created by Seah Park on 7/9/25.
+//
+
+import Testing
+@testable import SwiftDataProject
+
+struct SwiftDataProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
