09b573cbb11e387fd592b0924d9b7c0c3ff34f2a
seahpark247 <famousjedo@gmail.com>
2025-07-09 18:05:38 -0500

    feat: practice @Query(filter: #Predicate

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -5,17 +5,37 @@
 //  Created by Seah Park on 7/9/25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(filter: #Predicate<User> { user in
+        user.name.localizedStandardContains("E") && user.city == "London"
+    }, sort: \User.name) var users: [User]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(users) { user in                    Text(user.name)
+            }
+            .navigationTitle("Users")
+            .toolbar {
+                Button("Add Samples", systemImage: "plus") {
+                    try? modelContext.delete(model: User.self)
+                    
+                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                    let second = User(name: "Adele", city: "Paris", joinDate: .now.addingTimeInterval(86400 * -5))
+                    let third = User(name: "Taylor Swift", city: "New York", joinDate: .now.addingTimeInterval(86400 * 5))
+                    
+                    let fourth = User(name: "Justin Bieber", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+                    
+                    modelContext.insert(first)
+                    modelContext.insert(second)
+                    modelContext.insert(third)
+                    modelContext.insert(fourth)
+                }
+            }
         }
-        .padding()
     }
 }
 
--- SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Seah Park on 7/9/25.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
